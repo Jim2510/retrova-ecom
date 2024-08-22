9a7e35148d3698d88371c19318256eb4efc32ca7
Jim2510 <gmguglielmino@gmail.com>
2024-08-22 12:01:13 +0200

    new carousel + video starts

--- src/app/components/home-components/Crl.jsx
+++ src/app/components/home-components/Crl.jsx
@@ -1,107 +1,133 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { motion, Variants } from "framer-motion";
 import Image from "next/image";
-import { motion, AnimatePresence } from "framer-motion";
 import b1 from "../../../../public/images/1.png";
 import b2 from "../../../../public/images/2.png";
 import b3 from "../../../../public/images/3.png";
 import b1r from "../../../../public/images/1r.png";
 import b2r from "../../../../public/images/2r.png";
 import b3r from "../../../../public/images/3r.png";
+import { AnimatePresence } from "framer-motion";
 
 const desktopImages = [b1, b2, b3];
 const mobileImages = [b1r, b2r, b3r];
 
 export default function Crl() {
-  const [isMobile, setIsMobile] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
+  const [hideContainer, setHideContainer] = useState(false);
+  const [images, setImages] = useState(desktopImages);
 
   useEffect(() => {
+    // Funzione per aggiornare le immagini in base alla dimensione dello schermo
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
+      if (window.innerWidth <= 768) {
+        // Adatta questo valore per il breakpoint mobile
+        setImages(mobileImages);
+      } else {
+        setImages(desktopImages);
+      }
     };
 
-    handleResize();
+    // Aggiungi listener per il resize
     window.addEventListener("resize", handleResize);
+    // Imposta le immagini iniziali
+    handleResize();
 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+    // Pulisci il listener quando il componente viene smontato
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  const imageArray = isMobile ? mobileImages : desktopImages;
+  const variants = {
+    open: {
+      clipPath: "inset(0% 0% 0% 0%)",
+      transition: { duration: 1, ease: "easeInOut" },
+    },
+    closed: {
+      clipPath: "inset(50% 0% 50% 0%)",
+      transition: { duration: 1, ease: "easeInOut" },
+    },
+  };
 
-  // Aggiungi il video come ultima slide
-  const finalArray = [...imageArray, { type: "video", src: "/video/pres.mp4" }];
+  const handleClose = () => {
+    setIsVisible(false);
+  };
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === finalArray.length - 1 ? 0 : prevIndex + 1
-      );
-    }, 5000); // Cambia immagine ogni 5 secondi
-
-    return () => clearInterval(interval);
-  }, [finalArray]);
-
-  const handleSwipe = (direction) => {
-    if (direction === "left") {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === finalArray.length - 1 ? 0 : prevIndex + 1
-      );
-    } else if (direction === "right") {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === 0 ? finalArray.length - 1 : prevIndex - 1
-      );
+  const handleAnimationComplete = () => {
+    if (!isVisible) {
+      setHideContainer(true);
     }
   };
 
+  const handlePan = (event, info) => {
+    if (info.offset.x < -50) {
+      nextSlide();
+    } else if (info.offset.x > 50) {
+      prevSlide();
+    }
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
+  };
+
   return (
-    <div className="w-full h-screen relative mt-[2.5rem] overflow-hidden mb-4">
-      <AnimatePresence initial={false}>
-        {finalArray.map(
-          (item, index) =>
-            currentIndex === index && (
-              <motion.div
-                key={index}
-                className="absolute w-full h-full"
-                initial={{ opacity: 0, scale: 0.95 }}
-                animate={{ opacity: 1, scale: 1 }}
-                exit={{ opacity: 0, scale: 1.05 }}
-                transition={{ duration: 0.8 }}
-                onPanEnd={(e, { offset, velocity }) => {
-                  if (Math.abs(velocity.x) > 200) {
-                    handleSwipe(offset.x < 0 ? "left" : "right");
-                  }
-                }}
-              >
-                {item.type === "video" ? (
-                  <motion.video
-                    initial={{ opacity: 0, y: 50 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.8 }}
-                    viewport={{ once: true }}
-                    loop
-                    muted
-                    autoPlay
-                    className="w-full h-full object-cover"
-                  >
-                    <source type="video/mp4" src={item.src} />
-                  </motion.video>
-                ) : (
-                  <Image
-                    src={item}
-                    alt={`Slide ${index + 1}`}
-                    fill
-                    style={{ objectFit: "cover" }}
-                    className="w-full h-full"
-                  />
-                )}
-              </motion.div>
-            )
+    <>
+      <AnimatePresence>
+        {!hideContainer && isVisible && (
+          <motion.div
+            className="fixed top-0 z-50 w-full h-screen"
+            variants={variants}
+            initial="open"
+            animate="open"
+            exit="closed"
+            onClick={handleClose}
+            onAnimationComplete={handleAnimationComplete}
+          >
+            <video
+              autoPlay
+              muted
+              loop
+              controls={false}
+              className="w-full h-full object-cover"
+            >
+              <source src="/video/pres.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </motion.div>
         )}
       </AnimatePresence>
-    </div>
+
+      <div className="w-full h-screen relative overflow-hidden">
+        <motion.div
+          className="absolute inset-0 flex"
+          style={{ width: "100%", height: "100%" }}
+          animate={{ x: `-${currentIndex * 100}%` }}
+          transition={{ duration: 0.5 }}
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          onPan={handlePan}
+        >
+          {images.map((image, index) => (
+            <div className="w-full h-full flex-shrink-0 relative" key={index}>
+              <Image
+                src={image}
+                alt={`Slide ${index}`}
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+          ))}
+        </motion.div>
+      </div>
+    </>
   );
 }
