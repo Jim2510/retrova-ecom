c0e411800ceee17448798a6b8d5be1def4a4ca5e
Jim2510 <gmguglielmino@gmail.com>
2024-08-28 12:50:11 +0200

    footer style fixed

--- src/app/components/reusables/Ftr.jsx
+++ src/app/components/reusables/Ftr.jsx
@@ -13,9 +13,9 @@ import { motion } from "framer-motion";
 export default function Ftr() {
   return (
     <>
-      <div className="relative w-full min-h-[300px] grid grid-cols-5 bg-black text-white py-14 sm:text-center text-start">
+      <div className="relative w-full min-h-[300px] grid sm:grid-cols-5 grid-cols-2 bg-black text-white py-14 sm:text-center text-start">
         <div className="col-span-2 flex flex-col px-4 sm:px-28 py-2 sm:py-5 justify-center items-center">
-          <h3 className="font-semibold tracking-widest  sm:tracking-[1rem] text-base sm:text-xl">
+          <h3 className="font-semibold tracking-widest sm:tracking-[1rem] text-base sm:text-xl">
             RETROVA
           </h3>
           <div className="w-full py-2 h-full bg-white my-4 flex flex-col justify-center items-center shadow-2xl">
@@ -30,13 +30,14 @@ export default function Ftr() {
                 className="focus:outline-none border-2 border-black w-full sm:w-[40%] p-1 rounded-lg text-center"
                 placeholder="Your email"
               />
-              <button className=" py-2 px-2 sm:px-4 text-black border-2 border-black rounded-lg text-[0.65rem] sm:text-sm font-semibold">
+              <button className="py-2 px-2 sm:px-4 text-black border-2 border-black rounded-lg text-[0.65rem] sm:text-sm font-semibold">
                 SUBSCRIBE
               </button>
             </form>
           </div>
         </div>
-        <div className="col-span-1 text-xs sm:text-base sm:text-start text-center flex flex-col px-22 py-5">
+        {/* Sezione Info */}
+        <div className="sm:col-span-1 text-xs sm:text-base sm:text-start text-center flex flex-col px-4 sm:px-22 py-5">
           <h2 className="font-semibold tracking-wider">INFO</h2>
           <div className="py-4 flex flex-col sm:gap-7 gap-4 underline underline-offset-[2px]">
             <Link href="/info/faqs">FAQs</Link>
@@ -45,7 +46,8 @@ export default function Ftr() {
             <Link href="/info/contacts">Contact Us</Link>
           </div>
         </div>
-        <div className="col-span-1 text-xs sm:text-base sm:text-start text-center  flex flex-col px-22 py-5">
+        {/* Sezione Policy */}
+        <div className="sm:col-span-1 text-xs sm:text-base sm:text-start text-center flex flex-col px-4 sm:px-22 py-5">
           <h2 className="font-semibold tracking-wider">POLICY</h2>
           <div className="py-4 flex flex-col sm:gap-7 gap-4 underline underline-offset-[2px]">
             <Link href="/info/withdrawal">Withdrawal policy</Link>
@@ -54,7 +56,9 @@ export default function Ftr() {
             <Link href="/info/privacy">Privacy policy</Link>
           </div>
         </div>
-        <div className="col-span-1 text-xs sm:text-base sm:text-start text-center  flex flex-col px-22 py-5">
+        {/* Sezione Data */}
+
+        <div className="sm:col-span-1 col-span-2 text-xs sm:text-base sm:text-start text-center flex flex-col px-4 sm:px-22 sm:py-5">
           <h2 className="font-semibold tracking-wider">DATA</h2>
           <div className="py-4 flex flex-col sm:gap-7 gap-4 underline underline-offset-[2px]">
             <Link href="/info/faqs">Cookies</Link>
@@ -62,12 +66,12 @@ export default function Ftr() {
             <Link href="/info/contacts">Terms of sale</Link>
           </div>
         </div>
-        <div className="absolute text-sm sm:text-base  bottom-0 left-0 w-fit flex justify-center items-center p-2 gap-4">
+        <div className="absolute text-sm sm:text-base bottom-0 left-0 w-fit flex justify-center items-center p-2 gap-4">
           <Image src={x} alt="x" width={20} height={20} />
           <Image src={instagram} alt="instagram" width={20} height={20} />
           <Image src={facebook} alt="facebook" width={20} height={20} />
         </div>
-        <div className=" sm:text-base text-[0.60rem] absolute bottom-0 left-0 w-full flex justify-center items-center pt-2">
+        <div className="sm:text-base text-[0.60rem] absolute bottom-0 left-0 w-full flex justify-center items-center pt-2">
           <p>© 2024 Copyright: Retrova.it</p>
         </div>
         <div className="absolute top-0 left-0 w-full flex justify-end items-center pt-2 pr-5 gap-5">
@@ -92,51 +96,6 @@ export default function Ftr() {
           </div>
           <Image src={amex} alt="amex" width={35} height={35} />
         </div>
-        {/* <div className="border-r-[1px] border-white flex flex-col py-10 justify-between items-center">
-          <div className=""></div>
-          <div className="w-full">
-            <h2 className="w-full text-end pr-10 text-lg font-semibold mb-4">
-              ISCRIVITI ALLA NEWSLETTER
-            </h2>
-            <form className="flex justify-end w-full items-center gap-4 pr-10">
-              <input
-                type="text"
-                name=""
-                id="mail"
-                className="focus:outline-none my-4 border-2 border-secondary w-[40%] p-1 rounded-lg text-center"
-                placeholder="Inserisci qui la tua mail"
-              />
-              <button className=" py-2 px-4 bg-secondary/70 rounded-lg text-sm font-semibold">
-                ISCRIVITI
-              </button>
-            </form>
-          </div>
-        </div>
-        <ul className="grid grid-cols-3 py-8 justify-center items-center text-center text-sm font-semibold underline underline-offset-2 text-slate-600">
-          <Link href={"/faqs"} className="cursor-pointer">
-            FAQs
-          </Link>
-          <Link href={"/contacts"} className="cursor-pointer">
-            Contact Us
-          </Link>
-          <Link href={"/terms"} className="cursor-pointer">
-            Terms
-          </Link>
-          <div className="cursor-pointer">Link 4</div>
-          <div className="cursor-pointer">Link 5</div>
-          <div className="cursor-pointer">Link 6</div>
-          <div className="cursor-pointer">Link 7</div>
-          <div className="cursor-pointer">Link 9</div>
-          <div className="cursor-pointer">Link 10</div>
-          <div className="cursor-pointer">Link 11</div>
-          <div className="cursor-pointer">Link 12</div>
-          <div className="cursor-pointer">Link 13</div>
-        </ul>
-        <div className="absolute bottom-0 left-0 w-fit flex justify-center items-center p-2 gap-4">
-          <Image src={x} alt="x" width={20} height={20} />
-          <Image src={instagram} alt="instagram" width={20} height={20} />
-          <Image src={facebook} alt="facebook" width={20} height={20} />
-        </div> */}
       </div>
     </>
   );
